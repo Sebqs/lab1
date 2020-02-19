4714e6e310a5dbf2e4bc335ccf7537c5dfb606cb
Sebastian Ceballos <sebiyas2001@gmail.com>
2020-02-18 19:35:26 -0500

    test

---
+++ src/ui/lab1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class lab1 {
+	public static void main(String[] args){
+		Scanner reader = new Scanner(System.in);
+		
+		//Selección Modo Cadena, Numeros, Arreglos.
+		
+		System.out.println("Realizar actividades de (C)adena , (N)umeros o (A)rreglos:");
+		
+		
+	}
+}
